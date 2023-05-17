c4c803c077b06d9e9edaf884ce9d0fb3f039a9f5
lasinludwig <lasinludwig@gmail.com>
2023-05-17 17:02:35 +0200

    quick save

--- modules/excel_pl.py
+++ modules/excel_pl.py
@@ -5,6 +5,7 @@ import io
 import polars as pl
 from loguru import logger
 
+import modules.classes as cl
 import modules.general_functions as gf
 
 
@@ -14,12 +15,17 @@ def import_prefab_excel(file: io.BytesIO | None = None) -> pl.DataFrame:
     phil: io.BytesIO | str = (
         file or "example_files/Auswertung Stromlastgang - einzelnes Jahr.xlsx"
     )
+    mark_ind: str = cl.ExcelMarkers(cl.MarkerType.INDEX).marker_string
     df_messy: pl.DataFrame = pl.read_excel(
         phil,
         sheet_name="Daten",
         xlsx2csv_options={"skip_empty_lines": True, "skip_trailing_columns": True},
-        read_csv_options={"has_header": False},
+        read_csv_options={"has_header": False, "row_count_name": "row"},
     )  # type: ignore
     logger.info(df_messy.head())
 
+    for col in df_messy.columns:
+        if mark_ind in df_messy.select(pl.col(col)):
+            logger.info(f"Marker found in column '{col}'")
+    df_messy.row(by_predicate=pl.col("column_2") == "↓ Index ↓")
     return df_messy

cbdebffc8dfd792f2b4303d7baaf131107b0ded6
lasinludwig <lasinludwig@gmail.com>
2024-01-08 08:29:51 +0100

    qs

--- tests/test_st_graph.py
+++ tests/test_st_graph.py
@@ -1,13 +1,20 @@
 """Test the login page with the Streamlit testing framework"""
 
+import pathlib
+
+from plotly import graph_objects as go
 from streamlit.testing.v1 import AppTest
 
+from modules import classes_figs as clf
+from modules import constants as cont
 from modules import general_functions as gf
 from modules import setup_logger as slog
 from modules import setup_stuff
 
+EX_FILES = list(pathlib.Path("example_files").iterdir())
 
-def run_app_and_login() -> AppTest:
+
+def run_app_and_login(phil: pathlib.Path) -> AppTest:
     """Run the app on the given page"""
 
     at: AppTest = AppTest.from_file("pages/01_📈_Grafische_Datenauswertung.py")
@@ -22,11 +29,19 @@ def run_app_and_login() -> AppTest:
     # fake correct username and password
     at.session_state["authentication_status"] = True
 
-    return at.run()
+    # import Excel-file
+    at.session_state["f_up"] = phil
+
+    return at.run(timeout=60)
 
 
 def test_app_loading() -> None:
-    """Tests if the app starts without exception"""
-    at: AppTest = run_app_and_login()
+    """Tests if the app runs without exception"""
 
-    assert not at.exception
+    for file in EX_FILES:
+        at: AppTest = run_app_and_login(file)
+        figs: clf.Figs = at.session_state["figs"]
+        assert not at.exception
+        assert isinstance(figs.base, clf.FigProp)
+        assert isinstance(figs.base.fig, go.Figure)
+        assert figs.base.st_key == cont.FIG_KEYS.lastgang
